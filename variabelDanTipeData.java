51ecaca8657b1b852d5c99852da04316ea78859f
MI1412 <imronm1309@gmail.com>
2024-03-24 16:46:34 +0700

    variabel dan tipe data

---
+++ variabelDanTipeData.java
@@ -0,0 +1,31 @@
+public class variabelDanTipeData {
+// ini adalah komentar satu baris
+/*
+ini adalah komentar
+multiline
+ */
+    public static void main(String[] args) {
+// variabel di java
+        int a = 10 ; // tipe data integer
+        float tipeDataPecahan =  10 ;
+        byte tipeDataTerkecil = 10;
+        double tipeDataDouble = 10;
+        short e = 10;
+        long f = 10l;
+        boolean benar = true;
+        boolean salah = false;
+        String imron = "imron";
+        char tipeDataKarakter = 'c'; //berdasarkan ascii code
+        System.out.println("\ntipe data boolean benar adalah "+benar);
+        System.out.println("tipe data boolean salah adalah "+salah);
+        System.out.println("nilai tipe data int a = " + a);
+        System.out.println("nilai a x 10 = " + a*10);
+        System.out.println("tipe data float b = "+tipeDataPecahan);
+        System.out.println("tipe data byte c = "+ tipeDataTerkecil);
+        System.out.println("tipe data double d = "+ tipeDataDouble);
+        System.out.println("tipe data short e = "+ e);
+        System.out.println("tipe data long f = "+ f);
+        System.out.println("tipe data string imron adalah " + imron);
+        System.out.println("tipe data char c adalah " + tipeDataKarakter);
+    }
+}
